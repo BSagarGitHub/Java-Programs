ba70dec87588bc817e452230fbb4aca9e89d5b1d
BSagarGitHub <138560888+BSagarGitHub@users.noreply.github.com>
2023-07-04 13:15:24 +0530

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,16 @@
+import pack1.*;
+class demo1 extends ssss{
+void show5()
+{
+    show();
+}
+public static void main(String args[])
+{
+ssss obj1=new ssss();
+demo1 obj2=new demo1();
+obj2.show5();
+obj1.show3();
+obj1.show();
+ssss.show();
+}
+}
